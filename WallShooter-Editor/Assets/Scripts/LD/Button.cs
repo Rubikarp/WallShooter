a90ad03df2332a9c4e52ba8fb5d3354726f24a91
Deleye Arthur <arthurdeleye@gmail.com>
2020-08-04 21:54:36 +0200

    LD + script upgrade

--- WallShooter-Editor/Assets/Scripts/LD/Button.cs
+++ WallShooter-Editor/Assets/Scripts/LD/Button.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     [SerializeField] private List<Mechanisme> mechanismes = new List<Mechanisme>();
+    private bool activatable = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player/Bullet"))
+        if (collision.CompareTag("Player/Hurtbox") && !activatable)
+        {
+            Appuyer();
+        }
+
+        if (collision.CompareTag("Player/Bullet") &&!activatable)
         {
             Appuyer();
         }
@@ -15,9 +21,19 @@ public class Button : MonoBehaviour
 
     public void Appuyer()
     {
+        activatable = true;
+
         foreach (Mechanisme mecha in mechanismes)
         {
             mecha.Interact();
         }
+
+        Invoke("cooldown", 0.5f);
     }
+
+    public void cooldown()
+    {
+        activatable = false;
+    }
+
 }
---
+++ WallShooter-Editor/Assets/Scripts/Player/IntroScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour
+{
+    [SerializeField] private InputHandler input;
+    [SerializeField] private float startTime = 1.5f;
+    private void Awake()
+    {
+        input._canInput = false;
+        Invoke("InputActivate", startTime);
+    }
+
+    public void InputActivate()
+    {
+        input._canInput = true;
+    }
+}
